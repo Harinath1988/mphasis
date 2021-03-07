efde96e5c7be0affc4e8cb973bf5935ea55bb51d
Harinath <harisony61@gmail.com>
2021-03-07 14:55:12 +0530

    programs

---
+++ samples/src/main/java/org/example/SynchronizeMethodMain.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class SynchronizeMethodMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Synchronized Methods: ");
+        SynchronizeMethod worker2 = new SynchronizeMethod();
+        worker2.main();
+    }
+}
